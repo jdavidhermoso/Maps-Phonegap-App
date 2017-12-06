413434c00977b1fab9e1a7682bbf97b110e4b1dd
Juan David Hermoso <juandavidhermoso@gmail.com>
2017-12-06 11:19:36 +0100

    Get and print latitude and longitude

--- platforms/browser/www/js/maps.js
+++ platforms/browser/www/js/maps.js
@@ -4,11 +4,27 @@
         },
         initFastClick = function () {
             FastClick.attach(document.body);
-        };
+        },
+        onDeviceReady = function () {
+            navigator.geolocation.getCurrentPosition(paintCoords, coordsErrorHandler)
+        },
+        paintCoords = function (pos) {
+
+            var coordsElem = document.querySelector('#coords');
+            coordsElem.innerHTML = 'Lat: ' + pos.coords.latitude + ' Lon: ' + pos.coords.longitude;
+        },
+        coordsErrorHandler = function (err) {
+            console.log(err.code + ' ' + err.message);
+        }
+    ;
 
     if ('addEventListener' in document) {
         document.addEventListener('DOMContentLoaded', function () {
             init();
+        });
+
+        document.addEventListener('deviceready', function () {
+            onDeviceReady();
         });
     }
 })();
