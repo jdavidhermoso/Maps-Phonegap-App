ff016e8a223fd105e2f60d5fa7eec8c69d2fed03
Juan David Hermoso <juandavidhermoso@gmail.com>
2017-12-06 12:32:09 +0100

    Added map using leaflet

--- platforms/browser/www/js/maps.js
+++ platforms/browser/www/js/maps.js
@@ -9,22 +9,41 @@
             navigator.geolocation.getCurrentPosition(paintCoords, coordsErrorHandler)
         },
         paintCoords = function (pos) {
+            var myMap = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 13);
 
-            var coordsElem = document.querySelector('#coords');
-            coordsElem.innerHTML = 'Lat: ' + pos.coords.latitude + ' Lon: ' + pos.coords.longitude;
+            onMapLoaded();
+
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox.pirates',
+                accessToken: 'pk.eyJ1IjoiamRhdmlkaGVybW9zbyIsImEiOiJjamF1eDZzMXU2bmN2MzJxZTVzcTR0MTNuIn0.9xsIQGVMQo_G6gvzVDzoMg',
+            }).addTo(myMap);
+
+
         },
+
         coordsErrorHandler = function (err) {
             console.log(err.code + ' ' + err.message);
-        }
-    ;
+        },
+
+        onMapLoaded = function () {
+            var loadingMsg = document.querySelector('#loadingMsg'),
+                map = document.querySelector('#map');
+
+            loadingMsg.classList.add('hide');
+            loadingMsg.classList.remove('show');
+            map.classList.add('show');
+            map.classList.remove('hide');
+        };
 
     if ('addEventListener' in document) {
         document.addEventListener('DOMContentLoaded', function () {
             init();
-        });
+        }, false);
 
         document.addEventListener('deviceready', function () {
             onDeviceReady();
-        });
+        }, false);
     }
 })();
